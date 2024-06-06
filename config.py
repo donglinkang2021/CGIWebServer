134e348be37d69b4aad38479b55837b25f3eba78
Linkdom <donglinkang2021@163.com>
2024-06-06 20:39:37 +0800

    wrap server to a class

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 MAX_CONN = 10 # MAX_CONNECTIONS
 HOST, PORT = '0.0.0.0', 8888
 ADDR = (HOST, PORT)
-TIMEOUT = 60
+TIMEOUT = 30
 
 # webroot
 DOCUMENT_ROOT = './webroot'  # Web文档的根目录
--- server.py
+++ server.py
@@ -1,42 +1,52 @@
+import os
 import socket
 import threading
-from utils import addr2str
 from config import *
 
-# 处理客户端请求
-def handle_client(client_socket:socket.socket):
-    request = client_socket.recv(1024).decode('utf-8')
-    print(f"Received request:\n{request}")
-    
-    # 创建一个简单的HTTP响应
-    response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-    with open('hello.html', 'r') as f:
-        response += f.read()
-    client_socket.sendall(response.encode('utf-8'))
-    client_socket.close()
+def send_response(client:socket.socket, status_code:int):
+    status_str = f"{status_code} {HTTP_RESPONSES[status_code]}"
+    response = f"HTTP/1.1 {status_str}\r\nContent-Type: text/html\r\n\r\n"
+    client.sendall(response.encode('utf-8'))
 
-# 创建服务器套接字
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-server.listen(MAX_CONN)
-server.settimeout(TIMEOUT)
-print(f'Server listening on {addr2str(server.getsockname())}')
-host_name = socket.gethostbyname(socket.gethostname())
-print(f"Server's host name is {host_name}:{PORT}")
+def send_file(client:socket.socket, file_path:str):
+    if not os.path.exists(file_path):
+        send_response(client, 404)
+    else:
+        send_response(client, 200)
+        file_bytes = open(file_path, 'rb')
+        for line in file_bytes:
+            client.sendall(line)
 
-# 接受连接
-try:
-    while True:
-        client_sock, addr = server.accept()
-        print(f'Accepted connection from {addr}')
-        
-        # 创建一个新线程来处理请求
-        threading.Thread(
-            target=handle_client, 
-            args=(client_sock,), 
-            daemon=True
-        ).start()
+class HTTPServer:
+    def __init__(self, host:str, port:int, max_conn:int, timeout:int):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((host, port))
+        self.server.listen(max_conn)
+        self.server.settimeout(timeout)
+        self.host_name = socket.gethostbyname(socket.gethostname())
+        print(f"Server root: http://{self.host_name}:{port}/")
 
-except KeyboardInterrupt:
-    server.close()
-    print("\nServer stopped.")
+    def run(self):
+        try:
+            while True:
+                client, addr = self.server.accept()
+                print(f'Accepted connection from {addr}')
+                threading.Thread(
+                    target=self.handle_client, 
+                    args=(client,), 
+                    daemon=True
+                ).start()
+
+        except KeyboardInterrupt:
+            self.server.close()
+            print("\nServer stopped.")
+
+    def handle_client(self, client:socket.socket):
+        request = client.recv(1024).decode('utf-8')
+        print(f"Received request:\n{request}")
+        send_file(client, "hello.html")
+        client.close()
+
+if __name__ == '__main__':
+    server = HTTPServer(HOST, PORT, MAX_CONN, TIMEOUT)
+    server.run()
